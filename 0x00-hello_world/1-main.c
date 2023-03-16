f7becfb5be06103f4c3be00070e2ebe04e7baa7d
Alemami07 <chinwijemima2@gmail>
2023-03-16 10:10:27 -0700

    compilation

--- 0x00-hello_world/1-main.c
+++ 0x00-hello_world/1-main.c
@@ -1,1 +1,9 @@
-This is a C file
+/*Welcome to C*/
+#include <stdio.h>
+int main(void)
+{
+        string first_one = "Hello, World!"
+
+                printf('first_one' % s \n.");
+                return (0);
+}
