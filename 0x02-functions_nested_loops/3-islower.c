63ff62edaeb39e9bb6dce1553f7b16762c42cf77
Alemami07 <chinwijemima2@gmail>
2023-03-21 16:39:12 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,15 @@
 /**
  *_islower -  Entry point
  *Return: 1 if true,  0 if otherwise (success)
- *c: return value
+ *@c: return value
  *Description: write a C program that checks for lowercase
 */
 
 int _islower(int c)
-{	
+{
 	if (c >= 'a' && c <= 'z')
-	{	
-		return (1);
+	{
+	return (1);
 	}
 	else
 		return (0);
