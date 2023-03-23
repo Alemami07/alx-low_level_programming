0621a95f81d0a453458e255a4f8567b981e40292
Alemami07 <chinwijemima2@gmail>
2023-03-23 12:59:56 -0700

    updated multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,13 +6,12 @@
  *Description: write a C program that multiplies two integers
  *@a: first integer
  *@b: second integer
- *@c: return value
 */
 
 int mul(int a, int b)
 {
 	int c;
-	
+
 	c = (a * b);
 
 	return (c);
