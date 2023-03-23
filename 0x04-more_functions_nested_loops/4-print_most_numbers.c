12df14974b56d6b11dd1ff7a52a4c9d6a95e0b27
Alemami07 <chinwijemima2@gmail>
2023-03-23 14:56:04 -0700

    selected numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - Entry point
+ *Return: Always 0 (success)
+ *Description: write a C program that multiplies two integers
+*/
+
+void print_most_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+
+	if (c != '2' && c != '4')
+	{
+		_putchar(c);
+		c = c + 1; 
+	}
+	else 
+	{
+		c = c + 1;
+	}
+	{
+		_putchar('\n');
+	}
+}
