fa6d4b30237a7e47f5e49bbb5be7ffa63e7fc177
Alemami07 <chinwijemima2@gmail>
2023-03-23 12:56:51 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *mul-  Entry point
+ *Return: Aleays 0 (success)
+ *Description: write a C program that multiplies two integers
+ *@a: first integer
+ *@b: second integer
+ *@c: return value
+*/
+
+int mul(int a, int b)
+{
+	int c;
+	
+	c = (a * b);
+
+	return (c);
+}
