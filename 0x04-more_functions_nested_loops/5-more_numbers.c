468686a2d4f617633fd901195d1b1201d1e93142
Alemami07 <chinwijemima2@gmail>
2023-03-24 14:16:45 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *more_numbers - Entry point
+ *Return: Always 0 (success)
+ *Description: write a C program that prints the alphabet in lowercase
+*/
+void more_numbers(void)
+
+{
+	int n;
+
+	int c = 0;
+
+	while (c <= 9)
+	{
+	for (n = 0; n <= 14; n++)
+	{
+		_putchar(n);
+	}
+		_putchar('\n');
+	} 
+	c++;
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_line - Entry point
+ *Return: Always 0 (success)
+ *@n: return value
+ *Description: write a C program that prints lines
+*/
+
+void print_line(int n)
+{
+	for (n = 0; n <= 0; n++)
+	{
+		_putchar('\n');
+	}
+
+	n = 1;
+
+	while (n <= 10)
+	{
+		_putchar('_');
+		n++;
+	}
+		_putchar('\n');
+}
