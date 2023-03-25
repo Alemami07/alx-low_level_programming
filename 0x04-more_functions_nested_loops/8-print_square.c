3a7eadb31ee361f759997ead48eb20bf6aa7b86c
Alemami07 <chinwijemima2@gmail>
2023-03-25 14:52:11 -0700

    printing sqaures

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_square - Entry point
+ *@size: side of square
+ *Return value 0.
+ *Description: program thst prints sqaure
+ */
+
+void print_square(int size)
+{
+	int k, m;
+
+	for (k = 0; k < size; k++)
+	{
+	for (m = 0; m < size; m++)
+	{
+		_putchar('#');
+	}
+		_putchar('\n');
+	}
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+
+}
