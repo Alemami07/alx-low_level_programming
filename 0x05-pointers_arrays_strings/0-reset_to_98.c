b04bb166bffee1e404a543f2d40d77f990a658c6
Alemami07 <chinwijemima2@gmail>
2023-03-27 13:46:46 -0700

    updating value with pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - Entry point
+ *Return: Always 0. (success)
+ *@n: return value
+ *Description: write a program that updates the value
+ *of an integer through pointers.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
