140d7e857d6a1ec8e992723c3e147a8ce5a36467
Alemami07 <chinwijemima2@gmail>
2023-03-27 14:03:02 -0700

    swapping values with pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int - Entry point
+ *Return void.
+ *@a: first variable
+ *@b: second variable
+ *Description: write a program that swaps values
+ *of two variables
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+
+}
