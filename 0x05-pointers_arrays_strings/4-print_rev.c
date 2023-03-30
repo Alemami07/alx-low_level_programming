8bc255101d41900df6cec4b3122ef1db2d988c72
Alemami07 <chinwijemima2@gmail>
2023-03-30 03:54:41 -0700

    print half a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_rev - Entry point
+ *Return: no return
+ *@s: string to be reversed
+*/
+void print_rev(char *s)
+{
+	_strrev(s);
+
+	_putchar(s);
+	_putchar('\n');
+}
