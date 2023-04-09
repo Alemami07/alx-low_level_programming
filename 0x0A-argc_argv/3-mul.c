d424fc85365a6a950c5935944b9fdc40ef47d6f8
Alemami07 <chinwijemima2@gmail>
2023-04-09 15:18:12 -0700

    new count

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+int main(int argc, char *argv[])
+{
+	int p, q, prod;
+
+	for (p =  0; p < argc; p++)
+	{	
+	if (argc != 1)
+
+	else
+	{
+		printf("Error\n");
+		return (0);
+	}
+}
