2815d1ab778c64cbbd75d0f48819890716ffcb3e
Alemami07 <chinwijemima2@gmail>
2023-05-01 02:08:13 -0700

    frees lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *free_listint_t2 - frees list by making it NULL
+ *@head: points to list
+ *
+ *Return: NULL
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	}
+
+	*head = NULL;
+}
