e7a806c90e4297e421a09e07f9897e840c9e6983
Alemami07 <chinwijemima2@gmail.com>
2023-08-10 12:36:27 -0700

    .

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unsugned int n)
+{
+	char *new;
+	int l1, l2;;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	m = n = 0;
+	while (s1[m] != '\0')
+		l1++;
+	while (s2[n] != '\0')
+		l2++;
+}
