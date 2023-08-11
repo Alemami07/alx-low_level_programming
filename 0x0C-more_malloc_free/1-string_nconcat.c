e8b46a65004c1d0fae35aae51db6db661f7c936d
Alemami07 <chinwijemima2@gmail.com>
2023-08-11 07:21:20 -0700

    function that creates memory for an array

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -48,4 +48,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	new[u] = '\0';
 
 	return (new);
+
+	return (NULL);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_calloc - allocates memory for an array and initializes it to zero
+ *@nmemb: size of array
+ *@size: size of elements
+ *Return: pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int p;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+
+	if (arr == NULL)
+		return (NULL);
+	p = 0;
+	while (p < size)
+	{
+		arr[p] = 0;
+		p++;
+	}
+	return (arr);
+}
