5c9f2496f36b47f876f6d9695f77d63d5a5a3d5a
Alemami07 <chinwijemima2@gmail.com>
2023-08-16 18:38:59 -0700

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - entry
+ *@argc: counts
+ *@argv: points
+ *Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int byt, j;
+	char *bar;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+	exit(1);
+	}
+
+	byt = atoi(argv[1]);
+
+	if (byt < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	bar = (char *)main;
+
+	for (j = 0; j < byt; j++)
+	{
+		if (j == byt - 1)
+		{
+			printf("%02x\n", bar[j]);
+		}
+		printf("%02x\n", bar[j]);
+	}
+
+	return (0);
+}
