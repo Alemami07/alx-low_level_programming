cfddbcaf021c778dcc40545fb45b6b72d716513d
Alemami07 <chinwijemima2@gmail.com>
2023-08-17 05:42:52 -0700

    Var func that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ *print_numbers - prints integers passed
+ *@separator: seperates integers printed
+ *@n: number of integers passed
+ *Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int r;
+
+	if (n == 0)
+		return;
+	if (*separator == '\0')
+		separator = "";
+	va_start(args, n);
+
+	for (r = 0; r < n - 1; r++)
+	{
+		printf("%d", va_arg(args, int));
+		printf("%s", separator);
+	}
+	printf("%d\n", va_arg(args, int));
+}
