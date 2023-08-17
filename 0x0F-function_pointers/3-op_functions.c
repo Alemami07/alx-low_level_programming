29aec06148331f8a2c85226ad2b851eda8ec2791
Alemami07 <chinwijemima2@gmail.com>
2023-08-16 17:37:00 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "calc.h"
+/**
+ *op_add - adds two ints
+ *@a: first int
+ *@b:second int
+ *Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - subtracts
+ *@a: first
+ *@b: second
+ *Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - multiplies
+ *@a: first
+ *@b: second
+ *Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - divides
+ *@a: first
+ *@b: second
+ *Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - checks for remainder
+ *@a: first
+ *@b: second
+ *Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
